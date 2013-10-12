ca23cab1e312d674bf1f4350401a5a3a049b12a2
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2013-10-11 23:23:33 -0400

    Finished RESTfulness of the County
    All rest methods supported for the County.

--- apps/api/modules/api/ApiController.php
+++ apps/api/modules/api/ApiController.php
@@ -21,8 +21,9 @@ class ApiController extends Dinkly
 
 	public function loadCounty($params)
 	{
-		if(count($params) > 0)
-			$id = isset($params['id']) ? $params['id'] : $params[0];
+		$id = isset($params['id']) ? $params['id'] : NULL;
+		if ( is_null($id) )
+			unset($id);
 
 		$request = json_decode(file_get_contents('php://input'));
 
@@ -30,32 +31,104 @@ class ApiController extends Dinkly
 		switch($_SERVER['REQUEST_METHOD'])
 		{
 			case 'GET':
-				if (isset($id) ){
-					$county = new County();
-					$county->init($id);
-					$response = $county->to_json();
-				}else{
-					$collection = CountyCollection::getAll();
-					$response = json_encode(array_map(function($c){return $c->to_array();}, $collection));
-				}
+				if ( isset($id) ) 
+					{
+						$county = new County();
+						$county->init($id);
+						error_log($id);
+						$response = $county->to_json();
+					} 
+				else 
+					{
+						$collection = CountyCollection::getAll();
+						$response = json_encode(array_map(function($c){return $c->to_array();}, $collection));
+					}
 				break;
 
 			case 'POST':
-				$response = 'POST received!';
+				if ( isset( $request->name ) )
+					{
+						$county = new County();
+						$county->setName( $request->name );
+						error_log(print_r($request,1));
+						if( $county->save() )
+						{
+							$response = $county->to_json();
+						}
+						else
+						{	
+							header('HTTP/1.1 400 Bad Request');
+							$response = "{ \"error\" : \"Malformed Request\" }";
+						}
+					}
+				else
+					{
+						header('HTTP/1.1 400 Bad Request');
+						$response = "{ \"error\" : \"Malformed Request\" }";
+					}
+				
 				break;
 
 			case 'PUT':
-				$response = 'PUT received!';
+				if ( isset($id) ) 
+				{
+					if ( isset( $request->name ) )
+						{
+							$county = new County();
+							$county->init($id);
+
+							if( isset( $county->Id ) )
+								{
+									$county->setName( $request->name );
+									if( $county->save() )
+										$response = $county->to_json();
+									else
+										goto bad_request;
+								}
+							else
+								goto not_found;
+						}
+					else
+						goto bad_request;
+				}
+				else
+					goto bad_request;
 				break;
 
 			case 'DELETE':
-				$response = 'DELETE received!';
+				if ( isset($id ) )
+				{
+					$county = new County();
+					$county->init($id);
+					if( isset( $county->Id ) )
+						if ( $county->delete() )
+							$response = $county->to_json();
+						else
+							goto bad_request;
+					else
+						goto not_found;
+				}
 				break;
 		}
 
 		$this->handleResponse($response);
 
 		return false;
+
+		bad_request:
+			header('HTTP/1.1 400 Bad Request');
+			$response = "{ \"error\" : \"Bad Request\" }";
+			$this->handleResponse($response);
+			return false;
+
+		not_found:
+			header('HTTP/1.1 404 Not Found');
+			$response = "{ \"error\" : \"Could not find County\" }";
+			$this->handleResponse($response);
+			return false;
+
+
+		
 
 	}
 
