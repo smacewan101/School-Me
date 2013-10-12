16a41705d9320ec32638a9dee105bf66bbc8c78b
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2013-10-12 01:16:06 -0400

    Added REST resource for Dropout
    Added rest resource for Dropout, documented in README.md

---
+++ classes/models/custom/Dropout.php
@@ -0,0 +1,23 @@
+<?php
+
+class Dropout extends BaseDropout
+{
+	public function to_json()
+	{
+		return json_encode
+			(
+				$this->to_array()
+			);
+	}
+
+	public function to_array(){
+		return array(
+					'id' => $this->Id,
+					'year' => $this->Year,
+					'school_id' => $this->SchoolId,
+					'nine_2_twelve' => $this->Nine2Twelve,
+					'seven_2_twelve' => $this->Seven2Twelve
+					);
+	}
+}
+
