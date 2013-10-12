eb79d406ea17679de929edd514233623acae0503
Scott MacEwan <scott@bluehousegroup.com>
2013-10-11 20:34:38 -0400

    Goodbye db.yml

---
+++ apps/content/modules/main/views/default.php
@@ -0,0 +1,10 @@
+<div class="hero-unit">
+  <div>
+    <h1>
+      <?php echo Dinkly::getConfigValue('app_name'); ?>
+    </h1>
+    <p>
+      <?php echo Dinkly::getConfigValue('app_description'); ?>
+    </p>
+  </div>
+</div>
