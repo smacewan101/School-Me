9170e7804a59a9dcf75405104e8d2b3aab0944bf
Sean Perkins <perkinsean@gmail.com>
2013-10-12 03:06:52 -0400

    got map to fit screen better

--- apps/content/modules/main/views/map.php
+++ apps/content/modules/main/views/map.php
@@ -4,8 +4,8 @@
 <style type="text/css">
 	html { height: 100% }
 	body { height: 100%; margin: 0; padding: 0 }
-	#map-canvas { height: 800px }
-</style>
+	/*#map-canvas { height: 800px }
+*/</style>
 
 <div class='container-fluid'>
 	<div class='row-fluid'>
