390a5e3d555206c05925890d61e62bbdd9c8fffc
Sean Perkins <perkinsean@gmail.com>
2013-10-12 08:54:44 -0400

    fixed check box issue

--- apps/content/modules/main/views/map.php
+++ apps/content/modules/main/views/map.php
@@ -19,17 +19,9 @@ $(document).ready(function() {
 <script type="text/javascript">
 $(document).ready(function() {
 	$('#necaps').change(function() {
-		$('#mycheckboxdiv').toggle();
+		$('#mycheckboxdiv1').toggle();
 	});
 });
-</script>
-
-<!--populate drop-down menu -->	
-<script type="text/javascript" src="http://www.schoolme.dev/api/api/dropout/year/1">
-	$document.ready(function(){
-
-	});
-
 </script>
 
 <div class='container-fluid'>
@@ -46,9 +38,7 @@ $(document).ready(function() {
               			Select Year
               			<span class='caret'></span>
               			</a>
-              			<ul class='dropdown-menu'>
-             
-              		  
+              			<ul class='dropdown-menu'>  
 
               				<? foreach($dropout_years as $year):?>
               				<li><?=$year['year']?></li>
@@ -60,7 +50,7 @@ $(document).ready(function() {
               		</div>
 
               <li><label class='checkbox'><input type='checkbox' name='necaps' id='necaps' value=''>Necap Scores</label></li>
-              	<div id="mycheckboxdiv" style="display:none">
+              	<div id="mycheckboxdiv1" style="display:none">
               		<div class='btn-group'>
               			<a class='btn dropdown-toggle' data-toggle='dropdown' href="#">
               				Select Year
