5b9153cc0cdfc9d186e25303a84893be84aae00a
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2013-10-12 08:31:15 -0400

    Added Necap custom getAll method.
    Waiting on Evan's query to make it in there.

--- classes/models/custom/NecapCollection.php
+++ classes/models/custom/NecapCollection.php
@@ -2,6 +2,9 @@
 
 class NecapCollection extends BaseNecapCollection
 {
-
+	public static function getAll(){
+		$peer_object = new Necap();
+		return self::getCollection($peer_object, $peer_object->getSelectQuery() . " GROUP BY school_id LIMIT 100" );
+	}
 }
 
