5d259d43f503ef688636a89ef3beabfa4b5995a8
Sean Perkins <perkinsean@gmail.com>
2013-10-11 23:49:08 -0400

    more styling changes

--- apps/content/modules/main/views/default.php
+++ apps/content/modules/main/views/default.php
@@ -9,22 +9,24 @@
   </div>
 </div>
 
-<div class='row-fliud'>
-	<div class='span6'>
-		<form>
-			<fieldset>
-				<legend>Legend</legend>
-				<label>Select Filter</label>
-				<select>
-					<option>Counties</option>
-					<option>County0</option>
-					<option>County1</option>
-				</select>
-				<select>
-					<option>Schools</option>
-					<option>School1</option>
-				</select>
-			</fieldset>
-		</form>
+<div class='main-form'>
+	<div class='row-fliud'>
+		<div class='span4'>
+			<form>
+				<fieldset>
+					<legend>Legend</legend>
+					<label>Select Filter</label>
+					<select>
+						<option>Counties</option>
+						<option>County0</option>
+						<option>County1</option>
+					</select>
+					<select>
+						<option>Schools</option>
+						<option>School1</option>
+					</select>
+				</fieldset>
+			</form>
+		</div>
 	</div>
 </div>
