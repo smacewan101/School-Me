6f5002cc91746aabd1b0293d79473b35efad1e13
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2013-10-12 09:25:14 -0400

    Added institute info method:
    /api/api/institute/school_id/{school id}

--- classes/models/custom/InstituteCollection.php
+++ classes/models/custom/InstituteCollection.php
@@ -2,6 +2,13 @@
 
 class InstituteCollection extends BaseInstituteCollection
 {
-
+	public static function getSchoolInfo($sid){
+		$query = "select i.id,i.school_id,c.year,enrollment,eventcomplete,longitcomplete,name,district_id,nine2twelve,seven2twelve from completion c inner join institute i on c.school_id=i.school_id inner join dropout d on d.school_id = i.school_id where i.school_id='$sid' order by school_id asc;";
+		$db = self::fetchDB();
+		$stmt = $db->prepare($query);
+		$stmt->execute();
+		$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		return $result;
+	}
 }
 
